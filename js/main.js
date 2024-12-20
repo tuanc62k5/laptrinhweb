95c591e2d850792b98f487edad5402d9ccf8bb3b
tuanc62005 <dinhtuananh.dev@gmail.com>
2024-12-20 11:38:39 +0700

    sua lai nut cong tru

--- js/main.js
+++ js/main.js
@@ -1,27 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const quantityInput = document.querySelector(".quantity-control input");
-  const decreaseButton = document.querySelector(
-    ".quantity-control button:first-child"
-  );
-  const increaseButton = document.querySelector(
-    ".quantity-control button:last-child"
-  );
-
-  // Xử lý sự kiện khi nhấn nút giảm
-  decreaseButton.addEventListener("click", function () {
-    let currentQuantity = parseInt(quantityInput.value);
-    if (currentQuantity > 1) {
-      quantityInput.value = currentQuantity - 1;
-    }
-  });
-
-  // Xử lý sự kiện khi nhấn nút tăng
-  increaseButton.addEventListener("click", function () {
-    let currentQuantity = parseInt(quantityInput.value);
-    quantityInput.value = currentQuantity + 1;
-  });
-});
-
 // nav
 const menuToggle = document.getElementById("menuToggle");
 const navigation = document.querySelector(".navigation");
